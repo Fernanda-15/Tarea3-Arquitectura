69ae58c0437d1ffabc8921439aab8830e0579270
Fernanda-15 <fernandaqp15@gmail.com>
2024-11-01 16:08:29 -0600

    Configuracion de carrito

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@nuxt/image"],
   components: true,
   app: {
-    // Tags in head.
     head: {
-      // Title tag template.
       titleTemplate: `%s %separator %siteName %separator %class`,
       templateParams: {
-        siteName: 'Proyecto 1',
+        siteName: 'Tarea 3',
         class: 'Arquitectura de Información',
         separator: '|',
       },
-      // Meta tags.
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { hid: 'description', name: 'description', content: 'Meta description' }
       ],
-      // Link tags.
       link: [
         { rel: 'stylesheet', href: 'http://fonts.googleapis.com/css?family=Raleway:400,300,600' },
         { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', integrity: "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH", crossorigin: 'anonymous' },
@@ -30,9 +25,12 @@ export default defineNuxtConfig({
       script: [
         { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", integrity: "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz", crossorigin: "anonymous" },
         { src: 'https://cdn.snipcart.com/themes/v3.0.31/default/snipcart.js', async: true }
-
       ]
     },
   },
-
-})
+  runtimeConfig: {
+    public: {
+      snipcartApiKey: process.env.NUXT_PUBLIC_SNIPCART_API_KEY // Aquí accedemos a la variable de entorno
+    }
+  }
+});
