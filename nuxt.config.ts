ae5c91f3835b106295ce7f75110284752f019f21
Fernanda-15 <fernandaqp15@gmail.com>
2024-11-08 19:53:28 -0600

    Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
       ],
       script: [
         { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", integrity: "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz", crossorigin: "anonymous" },
-        { src: 'https://cdn.snipcart.com/themes/v3.0.31/default/snipcart.js', async: true }
+        { src: 'https://cdn.snipcart.com/themes/v3.0.31/default/snipcart.js', async: true, defer: true }
       ]
     },
   },
